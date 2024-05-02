96a3dcc8c581e6ae7d442f882956504f62c764c9
ruuffian <liam.mkay1250@gmail.com>
2024-05-02 15:53:43 -0400

    Initial settings commit

---
+++ lua/ruuffian/set.lua
@@ -0,0 +1,26 @@
+local set  = vim.opt
+
+set.mouse = ""
+set.guicursor = ""
+
+set.nu = true
+set.relativenumber = true
+
+-- Indentation
+set.tabstop = 2
+set.shiftwidth = 2
+set.softtabstop = 2
+set.expandtab = true
+set.smarttab = false
+set.smartindent = true
+
+set.hlsearch = false
+set.incsearch = true
+
+set.termguicolors = true
+
+set.scrolloff = 10
+
+set.wrap = true
+set.textwidth = 80
+set.colorcolumn = "81"
