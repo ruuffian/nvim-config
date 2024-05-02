ae67c6654d5479ee3baf332d6764ee7323c0854a
ruuffian <liam.mkay1250@gmail.com>
2024-05-02 15:53:33 -0400

    Initial remap commit

---
+++ lua/ruuffian/remap.lua
@@ -0,0 +1,8 @@
+-- NetRW
+vim.api.nvim_set_keymap('n', '<leader>pv', ':Ex<CR>', {})
+
+-- Panel Navigation
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {})
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {})
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {})
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {})
