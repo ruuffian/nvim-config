41dd234879cc9e3d87533e94951f9f1009eaac11
ruuffian <ruuffian@gmail.com>
2025-01-18 20:38:49 -0500

    Missed lua syntax.

--- lua/ruuffian/plugins/telescope.lua
+++ lua/ruuffian/plugins/telescope.lua
@@ -5,13 +5,13 @@ return {
     'devicons',
   },
   config = function()
-    local builtin = require('telescope.builtin')
+    local builtin = require 'telescope.builtin'
     local set = vim.keymap.set
     -- Basic Nav
     set('n', '<leader>pf', builtin.find_files, {})
     set('n', '<C-p>', builtin.git_files, {})
     set('n', '<leader>ps', function()
-      builtin.grep_string({ search = vim.fn.input('grep ')});
+      builtin.grep_string { search = vim.fn.input '> '} 
     end, {})
     set('n', '<C-f>', builtin.live_grep, {})
   end
