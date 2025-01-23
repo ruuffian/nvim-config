8a3c53913cda0174ce25879c5e2d0de94d404d7a
ruuffian <ruuffian@gmail.com>
2025-01-23 15:03:41 -0500

    Fixed treesitter config, FINALLY.

--- lua/ruuffian/plugins/treesitter.lua
+++ lua/ruuffian/plugins/treesitter.lua
@@ -6,16 +6,19 @@ local installed = {
 return {
   'nvim-treesitter/nvim-treesitter',
   build = 'TSUpdate',
-  opts = {
-    ensure_installed = installed,
-    sync_install = false,
-    auto_install = true,
-    indent = {
-      enable = true,
-    },
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-  },
+  config = function()
+    local configs = require 'nvim-treesitter.configs'
+    configs.setup {
+      ensure_installed = installed,
+      sync_install = false,
+      auto_install = true,
+      indent = {
+        enable = true,
+      },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = true,
+      },
+    }
+  end
 }
