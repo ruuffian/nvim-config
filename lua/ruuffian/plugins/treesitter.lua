6a3d4bf2515d40293854d69ae7680e800c8a6edf
ruuffian <ruuffian@gmail.com>
2025-01-23 15:26:00 -0500

    Added support for blade templates to treesitter

--- lua/ruuffian/plugins/treesitter.lua
+++ lua/ruuffian/plugins/treesitter.lua
@@ -7,6 +7,22 @@ return {
   'nvim-treesitter/nvim-treesitter',
   build = 'TSUpdate',
   config = function()
+    -- Blade Templates
+    vim.filetype.add({
+      pattern = {
+        ['.*%.blade%.php'] = 'blade',
+      },
+    })
+    local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+    parser_config.blade = {
+      install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+      },
+      filetype = "blade"
+    }
+
     local configs = require 'nvim-treesitter.configs'
     configs.setup {
       ensure_installed = installed,
